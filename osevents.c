1a3d15bd47cd6427656b453522f2af9dbe9d88ef
SamirVojdani <Samir.Vojdani@gmail.com>
2016-09-08 01:49:01 +0800

    added author names and id. numbers

--- osevents.c
+++ osevents.c
@@ -1,1 +1,12 @@
+/* CITS2002 Project 1 2016
+Names: Samir Vojdani, Kwang Zhu Ong
+Student numbers: 21567881, 21540729
+Date: date-of-submission
+*/
 
+#include <stdlib.h>
+
+int main (void) {
+
+exit(EXIT_SUCCESS);
+}
